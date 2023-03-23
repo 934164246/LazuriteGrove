e91d206e9f3a174e95b85993cf92a7930effd792
gokoururi <934164246@qq.com>
2023-03-23 18:02:40 +0800

    初始化

---
+++ lazurite-grove-back/src/main/java/com/rurigokou/back/LazuriteGroveBackApplication.java
@@ -0,0 +1,14 @@
+package com.rurigokou.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author gokoururi
+ */
+@SpringBootApplication
+public class LazuriteGroveBackApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LazuriteGroveBackApplication.class, args);
+    }
+}
---
+++ lazurite-grove-front/src/main/java/com/rurigokou/front/LazuriteGroveFrontApplication.java
@@ -0,0 +1,14 @@
+package com.rurigokou.front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author gokoururi
+ */
+@SpringBootApplication
+public class LazuriteGroveFrontApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LazuriteGroveFrontApplication.class, args);
+    }
+}
