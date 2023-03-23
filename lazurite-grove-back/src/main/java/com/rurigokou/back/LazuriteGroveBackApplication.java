7245168a20c4b03b9026122267bd44635f36c3cc
gokoururi <934164246@qq.com>
2023-03-24 00:29:15 +0800

    整合nacos

--- lazurite-grove-back/src/main/java/com/rurigokou/back/LazuriteGroveBackApplication.java
+++ lazurite-grove-back/src/main/java/com/rurigokou/back/LazuriteGroveBackApplication.java
@@ -2,12 +2,15 @@ package com.rurigokou.back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author gokoururi
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class LazuriteGroveBackApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(LazuriteGroveBackApplication.class, args);
     }
--- lazurite-grove-front/src/main/java/com/rurigokou/front/LazuriteGroveFrontApplication.java
+++ lazurite-grove-front/src/main/java/com/rurigokou/front/LazuriteGroveFrontApplication.java
@@ -2,10 +2,12 @@ package com.rurigokou.front;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author gokoururi
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class LazuriteGroveFrontApplication {
     public static void main(String[] args) {
